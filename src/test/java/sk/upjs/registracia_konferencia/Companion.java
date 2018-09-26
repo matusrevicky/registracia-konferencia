6e30cc4923309857501723b7b77e6b5e1d616372
student <student@158.197.35.82>
2018-09-26 09:35:17 +0200

    initial commit 2

--- src/test/java/sk/upjs/registracia_konferencia/Companion.java
+++ src/test/java/sk/upjs/registracia_konferencia/Companion.java
@@ -1,5 +1,12 @@
 package sk.upjs.registracia_konferencia;
 
+import java.time.LocalDateTime;
+
 public class Companion {
+	
+	private CompanionCategory category;
+	private LocalDateTime start;
+	private LocalDateTime end;
+	private Tshirt tshirt;
 
 }
--- src/test/java/sk/upjs/registracia_konferencia/Workshop.java
+++ src/test/java/sk/upjs/registracia_konferencia/Workshop.java
@@ -1,5 +1,15 @@
 package sk.upjs.registracia_konferencia;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Workshop {
+	private String name;
+	private LocalDate start;
+	private LocalDate end;
+	private double priceFull;
+	private double priceStudent;
+	private double priceFullLate;
+	private double priceStudentLate;
 
 }
