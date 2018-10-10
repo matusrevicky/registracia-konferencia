ffc1104fe1b8e225ef5abf254c79dd640d789652
student <student@158.197.35.82>
2018-10-10 07:28:57 +0200

    commit

--- src/main/java/sk/upjs/registracia_konferencia/App.java
+++ src/main/java/sk/upjs/registracia_konferencia/App.java
@@ -1,13 +1,32 @@
 package sk.upjs.registracia_konferencia;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		ParticipantsListController mainController = new ParticipantsListController();
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ParticipantsList.fxml"));
+		fxmlLoader.setController(mainController);
+		Parent rootPane = fxmlLoader.load();
+
+		Scene scene = new Scene(rootPane);
+		primaryStage.setTitle("Organizacia konferencie");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
